d4f90aaf1e5587af45766dbc5c7a28acdc6f71b6
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-08-15 17:44:01 -0300

    feet: create table users

---
+++ src/main/resources/db/migration/V1__create-users-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) UNIQUE,
+    password   TEXT                NOT NULL,
+    avatar_url TEXT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
