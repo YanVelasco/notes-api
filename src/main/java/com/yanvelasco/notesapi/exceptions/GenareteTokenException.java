6b251f31e5d1fb8751e300c29b21e88fa07a4b07
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-08-16 14:18:22 -0300

    feet: generate token exception exception

---
+++ src/main/java/com/yanvelasco/notesapi/exceptions/GenareteTokenException.java
@@ -0,0 +1,7 @@
+package com.yanvelasco.notesapi.exceptions;
+
+public class GenareteTokenException extends Exception {
+    public GenareteTokenException(String message) {
+        super(message);
+    }
+}
