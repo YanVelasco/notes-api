47bbbfed546705fd2771363af1fd03c2371569e0
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-08-17 12:07:43 -0300

    feet: crete method to search a note by id

--- src/main/java/com/yanvelasco/notesapi/domain/notes/repository/NoteRepository.java
+++ src/main/java/com/yanvelasco/notesapi/domain/notes/repository/NoteRepository.java
@@ -3,6 +3,12 @@ package com.yanvelasco.notesapi.domain.notes.repository;
 import com.yanvelasco.notesapi.domain.notes.entity.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<NoteEntity, String> {
     boolean existsByTitle(String title);
+
+    List<NoteEntity> findByUserIdOrderByTitleAsc(String userId);
+
+    List<NoteEntity> findByUserIdAndTitleContainingIgnoreCaseOrderByTitleAsc(String userId, String title);
 }
