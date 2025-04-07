a6293b5ae719329ca36d3f0183de8999ddf18197
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-04-07 18:51:00 -0300

    Refactor application configuration and add Docker Compose setup for PostgreSQL

--- src/main/resources/db/migration/V5__Insert_Data_Into_Users.sql
+++ src/main/resources/db/migration/V5__Insert_Data_Into_Users.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (id, username, email, password, account_non_expired, account_non_locked, credentials_non_expired,
                    enabled, credentials_non_expiry_date, account_expiry_date, two_factor_enabled, sign_up_method,
                    role_id, created_at, updated_at)
-VALUES (gen_random_uuid(), 'Yan', 'yan@example.com', '{noop}password', TRUE, TRUE, TRUE, TRUE,
+VALUES (gen_random_uuid(), 'Yan', 'yan@example.com', '$2y$10$lDuyo4Fr52hf7gqzmdI4uOg98sAsfjnuXT/WR8Ze7bQrapEbVbDN.', TRUE, TRUE, TRUE, TRUE,
         CURRENT_TIMESTAMP + INTERVAL '1 year', CURRENT_TIMESTAMP + INTERVAL '1 year', FALSE, 'email',
         (SELECT id FROM roles WHERE role_name = 'ROLE_ADMIN'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
